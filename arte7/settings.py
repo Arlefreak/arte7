76be2d8ae53e6cf24b6922d31a6e5a6f99d982c7
afk <afk@ellugar.co>
2018-10-01 13:29:21 -0500

    Fix url admin

--- arte7/settings.py
+++ arte7/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'cms',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
